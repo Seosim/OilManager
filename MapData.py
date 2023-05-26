5b9a23683074d233470d1b3f550fab131992e851
Seosim <103580344+Seosim@users.noreply.github.com>
2023-05-26 14:39:22 +0900

    MapData 클래스화

--- MapData.py
+++ MapData.py
@@ -5,37 +5,50 @@ import io
 import OilData
 from KeyData import *
 
+class Map:
+    def __init__(self):
+        self.endpoint = "https://naveropenapi.apigw.ntruss.com/map-static/v2/raster"
+        self.headers = {
+            "X-NCP-APIGW-API-KEY-ID": client_id,
+            "X-NCP-APIGW-API-KEY": client_secret,
+        }
+        # 중심 좌표
+        self.lon, self.lat = OilData.oilAPI.gasStationList[0].getPos()
+        self.center = f"{self.lon},{self.lat}"
+        # 줌 레벨 - 0 ~ 20
+        self.level = 14
+        # 가로 세로 크기 (픽셀)
+        self.w, self.h = 300, 300
+        # 지도 유형 - basic, traffic, satellite, satellite_base, terrain
+        self.maptype = "basic"
+        # 반환 이미지 형식 - jpg, jpeg, png8, png
+        self.format = "png"
+        # 고해상도 디스펠레이 지원을 위한 옵션 - 1, 2
+        self.scale = 1
+        # 마커
+        self.markers = f"""type:d|size:mid|pos:{self.lon} {self.lat}|color:red"""
+        # 라벨 언어 설정 - ko, en, ja, zh
+        self.lang = "ko"
+        # 대중교통 정보 노출 - Boolean
+        self.public_transit = True
+        # 서비스에서 사용할 데이터 버전 파라미터 전달 CDN 캐시 무효화
+        self.dataversion = ""
 
-endpoint = "https://naveropenapi.apigw.ntruss.com/map-static/v2/raster"
-headers = {
-    "X-NCP-APIGW-API-KEY-ID": client_id,
-    "X-NCP-APIGW-API-KEY": client_secret,
-}
-# 중심 좌표
-lon, lat = OilData.oilAPI.gasStationList[0].getPos()
-_center = f"{lon},{lat}"
-# 줌 레벨 - 0 ~ 20
-_level = 14
-# 가로 세로 크기 (픽셀)
-_w, _h = 300, 300
-# 지도 유형 - basic, traffic, satellite, satellite_base, terrain
-_maptype = "basic"
-# 반환 이미지 형식 - jpg, jpeg, png8, png
-_format = "png"
-# 고해상도 디스펠레이 지원을 위한 옵션 - 1, 2
-_scale = 1
-# 마커
-_markers = f"""type:d|size:mid|pos:{lon} {lat}|color:red"""
-# 라벨 언어 설정 - ko, en, ja, zh
-_lang = "ko"
-# 대중교통 정보 노출 - Boolean
-_public_transit = False
-# 서비스에서 사용할 데이터 버전 파라미터 전달 CDN 캐시 무효화
-_dataversion = ""
+        #이미지 데이터
+        self.image = None
 
-# URL
-url = f"{endpoint}?center={_center}&level={_level}&w={_w}&h={_h}&maptype={_maptype}&format={_format}&scale={_scale}&markers={_markers}&lang={_lang}&public_transit={_public_transit}&dataversion={_dataversion}"
-res = requests.get(url, headers=headers)
+    def GetImage(self, idx):
+        self.lon, self.lat = OilData.oilAPI.gasStationList[idx].getPos()
+        self.center = f"{self.lon},{self.lat}"
+        self.markers = f"""type:d|size:mid|pos:{self.lon} {self.lat}|color:red"""
 
-image_data = io.BytesIO(res.content)
-image = Image.open(image_data)
+        # URL
+        url = f"{self.endpoint}?center={self.center}&level={self.level}&w={self.w}&h={self.h}&maptype={self.maptype}&format={self.format}&scale={self.scale}&markers={self.markers}&lang={self.lang}&public_transit={self.public_transit}&dataversion={self.dataversion}"
+        res = requests.get(url, headers=self.headers)
+
+        image_data = io.BytesIO(res.content)
+        self.image = Image.open(image_data)
+        return self.image
+
+map = Map()
+
