64cdf91aae57e289e0a12b1dba9d5b9ca541656d
inhogo(2019180003) <dlsghdmsqlc@naver.com>
2023-05-27 15:07:18 +0900

    막대 도표에 상세가격 표시

--- TermProject(0003, 0018).py
+++ TermProject(0003, 0018).py
@@ -38,11 +38,12 @@ class Program:
             count = 0
             for i in OilData.oilAPI.localPrice:
                 if i == minlocal:
-                    color = 'red3'
+                    color = 'tomato'
                 else:
                     color = 'pale green'
                 self.Chart.create_rectangle(110 + 30 * count, ChartHeight - ((float)(OilData.oilAPI.localPrice[i]['경유'][0]) - minprice) - 100, 130 + 30 * count, ChartHeight - 20, fill = color, tags='Chart')        
                 tk.Label(self.Chart, text=i, font=self.mfont).place(x=105 + 30 * count, y= ChartHeight-20)
+                tk.Label(self.Chart, text=OilData.oilAPI.localPrice[i]['경유'][0], font=self.chartfont).place(x=105 + 30 * count, y= ChartHeight - ((float)(OilData.oilAPI.localPrice[i]['경유'][0]) - minprice) - 120)
                 count += 1
         # -------------------------------------- 전국 경유 막대그래프 ---------------------------------------------
         elif kind == 'gasoline':
@@ -61,11 +62,12 @@ class Program:
             count = 0
             for i in OilData.oilAPI.localPrice:
                 if i == minlocal:
-                    color = 'red3'
+                    color = 'tomato'
                 else:
                     color = 'pale green'
                 self.Chart.create_rectangle(110 + 30 * count, ChartHeight - ((float)(OilData.oilAPI.localPrice[i]['휘발유'][0]) - minprice) - 100, 130 + 30 * count, ChartHeight - 20, fill = color, tags='Chart')        
                 tk.Label(self.Chart, text=i, font=self.mfont).place(x=105 + 30 * count, y= ChartHeight-20)  # , tags='Chart'
+                tk.Label(self.Chart, text=OilData.oilAPI.localPrice[i]['휘발유'][0], font=self.chartfont).place(x=105 + 30 * count, y= ChartHeight - ((float)(OilData.oilAPI.localPrice[i]['휘발유'][0]) - minprice) - 120)
                 count += 1
         # -------------------------------------- 전국 휘발유 막대그래프 ---------------------------------------------
         elif kind == "premiumgasoline":
@@ -84,11 +86,12 @@ class Program:
             count = 0
             for i in OilData.oilAPI.localPrice:
                 if i == minlocal:
-                    color = 'red3'
+                    color = 'tomato'
                 else:
                     color = 'pale green'
                 self.Chart.create_rectangle(110 + 30 * count, ChartHeight - ((float)(OilData.oilAPI.localPrice[i]['고급 휘발유'][0]) - minprice) - 100, 130 + 30 * count, ChartHeight - 20, fill = color, tags='Chart')        
                 tk.Label(self.Chart, text=i, font=self.mfont).place(x=105 + 30 * count, y= ChartHeight-20)  # , tags='Chart'
+                tk.Label(self.Chart, text=OilData.oilAPI.localPrice[i]['고급 휘발유'][0], font=self.chartfont).place(x=105 + 30 * count, y= ChartHeight - ((float)(OilData.oilAPI.localPrice[i]['고급 휘발유'][0]) - minprice) - 120)
                 count += 1
         # -------------------------------------- 전국 고급 휘발유 막대그래프 ---------------------------------------------      
         self.Chart.create_rectangle(50, ChartHeight - 40, ChartWidth - 50, ChartHeight - 20, fill = 'black', tags='Chart')  
@@ -103,6 +106,7 @@ class Program:
         self.window.title("기름값 도우미")
         self.font = "Arial 15"
         self.mfont = "Arial 10"
+        self.chartfont = "Arial 6"
         self.favoriteList = []
         self.selectedOilStation = None
 
@@ -111,7 +115,6 @@ class Program:
         self.frame1 = tk.Frame(self.window)
         self.notebook.add(self.frame1, text="ONE")
         self.notebook.pack()
-        # 오늘 기름 가격 그래프 표시 테스트 - 서종배
 
         # 기름 선택 버튼    - 고인호
         self.disselbutton = tk.Button(self.frame1, text='경유', command=lambda row="dissel": self.DrawChart(row))
@@ -122,40 +125,39 @@ class Program:
 
         self.premiumgasolinebutton = tk.Button(self.frame1, text='고급 휘발유', command=lambda row="premiumgasoline": self.DrawChart(row))
         self.premiumgasolinebutton.place(x=Width/2 + 10, y = 50)
-        # 기름 선택 버튼    - 고인호
 
+        # Title
         self.n1Title = tk.Label(self.frame1, text="오늘의 기름값", font=self.font)
         self.n1Title.place(x=Width / 2 - 75, y= 0)
 
+        # 지도 나오는 프레임
         self.frame2 = tk.Frame(self.window)
         self.notebook.add(self.frame2, text="TWO")
         self.notebook.pack()
 
-        #-----------------------------상단에 글씨 띄우는 캔버스--------------------------------         START
+        # 상단에 글씨 띄우는 캔버스    - 고인호
         MainCanvas = tk.Canvas(self.frame2, bg = 'white', width=Width, height=Height/20)
         MainCanvas.pack()
         MainCanvas.create_text(Width / 2, Height / 40, text = '지역 주유소 최저가 검색')
-        #-----------------------------상단에 글씨 띄우는 캔버스--------------------------------         END
 
 
-        #----------------------------기름 종류, 지도 그리는 프레임---------------------------------     START
+        # 기름 종류, 지도 그리는 프레임    - 고인호
         frame1 = tk.Frame(self.frame2)
         frame1.pack(side=tk.TOP)
-        #------------------------------지도를 그리는 캔버스-----------------------------------          START
+
+        # 지도를 그리는 캔버스    - 고인호
         photo = ImageTk.PhotoImage(MapData.map.GetImage())   #지도이미지
         mapLabel = tk.Label(frame1, image=photo, width=MapWidth, height=MapHeight)
         mapLabel.pack(side=tk.RIGHT)
-        #------------------------------지도를 그리는 캔버스-----------------------------------          END
 
-        #-------------------------------지역 선택하는 콤보박스---------------------------------         START
+        # 지역 선택하는 콤보박스    - 고인호
         # selected_gu = tk.StringVar()
         # selected_gu.set("시흥시")  # 초기값 설정
         # gu_options = set([gasstations['address'].split()[1] for hospital in hospitals])
         # gu_combo = ttk.Combobox(root, textvariable=selected_gu, values=list(gu_options))
         # gu_combo.pack()
-        #-------------------------------지역 선택하는 콤보박스---------------------------------         END
 
-        #-------------------------------기름 종류 선택하는 박스---------------------------------        START
+        # 기름 종류 선택    - 고인호
         # 체크 박스
         tk.Checkbutton(frame1, text='경유', command=self.up).pack(side=tk.TOP)
         tk.Checkbutton(frame1, text='휘발유', command=self.up).pack(side=tk.TOP)
@@ -164,18 +166,12 @@ class Program:
         # 버튼
         tk.Button(frame1, text="검색", command=self.search).pack(side=tk.TOP)
 
-        #-------------------------------기름 종류 선택하는 박스---------------------------------        END
-
-        #----------------------------기름 종류, 지도 그리는 프레임---------------------------------     END
-
-        #----------------------------기름값 도표 그리는 캔버스---------------------------------         START
+        # 기름값 도표 그리는 캔버스    - 고인호
         Chart = tk.Canvas(self.frame2, width=ChartWidth, height=ChartHeight)
         Chart.pack(side=tk.TOP)
 
         Chart.create_rectangle(100, ChartHeight-70, ChartWidth - 100, ChartHeight-50, fill = 'black', tags='Chart')
         # tk.Button(Chart, text="검색", command=self.search).pack(side=tk.TOP)
-        #----------------------------기름값 도표 그리는 캔버스---------------------------------         END
-
 
 
         self.window.mainloop()
