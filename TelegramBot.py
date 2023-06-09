455508c0c817a401208ada1576bd7bbcaa641fdb
Seosim <103580344+Seosim@users.noreply.github.com>
2023-06-09 18:48:38 +0900

    텔레그램 명령어 즐겨찾기 추가

--- TelegramBot.py
+++ TelegramBot.py
@@ -10,17 +10,35 @@ def handle(msg):
 
     text = msg["text"]
     args = text.split()
-    for code in OilData.oilAPI.GuCodeList.keys():
-        if args[1] in OilData.oilAPI.GuCodeList[code]:
-            local = OilData.oilAPI.GuCodeList[code][args[1]]
-    OilData.oilAPI.TelegramCheap(local, args[2])
+
+    if args[0] == '지역최저가':
+        if len(args) < 2: args.append("구로구")
+        if len(args) < 3: args.append("경유")
+
+
 
     message = ""
 
     if args[0] == "지역최저가":
         message += args[1] + " 의 최저가 주유소 정보 입니다.\n\n"
+        for code in OilData.oilAPI.GuCodeList.keys():
+            if args[1] in OilData.oilAPI.GuCodeList[code]:
+                local = OilData.oilAPI.GuCodeList[code][args[1]]
+        OilData.oilAPI.TelegramCheap(local, args[2])
         for gas in OilData.oilAPI.telegramStation:
             oilstation = gas.getInfo()
+            message += "주유소 이름 : " + oilstation['OS_NM'] + '\n' + "도로명 주소 : " + oilstation['NEW_ADR'] + '\n' \
+                    + "구 주소명 : " + oilstation['VAN_ADR'] + '\n'
+            for price in oilstation['OIL_PRICE']:
+                if price['PRODCD'] == 'D047' : message += "경유 가격 : " + price['PRICE'] + '\n'
+                elif price['PRODCD'] == 'B027': message += "휘발유 가격 : " + price['PRICE'] + '\n'
+            message += "세차장 유무 : " + oilstation['CAR_WASH_YN'] + '\n'
+            message += "편의점 유무 : " + oilstation['CVS_YN'] + '\n\n\n\n'
+        bot.sendMessage("5719105561", message)
+    elif args[0] == '즐겨찾기':
+        message += " 즐겨찾기된 주유소 정보입니다.\n\n"
+        for gas in OilData.oilAPI.saveOilStation:
+            oilstation = OilData.oilAPI.saveOilStation[gas].getInfo()
             message += "주유소 이름 : " + oilstation['OS_NM'] + '\n' + "도로명 주소 : " + oilstation['NEW_ADR'] + '\n' \
                     + "구 주소명 : " + oilstation['VAN_ADR'] + '\n'
             for price in oilstation['OIL_PRICE']:
