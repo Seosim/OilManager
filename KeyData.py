a3d20544f61c619fd29fb6419ce88c40b940afb1
inho.go(2019180003) <dlsghdmsqlc@naver.com>
2023-06-11 03:06:55 +0900

    key형식

---
+++ KeyData.py
@@ -0,0 +1,8 @@
+client_id = "//"
+client_secret = "//"
+
+OilAPIcode = "기름 api입력"       #커밋할때 항상 지울 것
+kakaoAPIcode = "카카오 api입력"       #커밋할때 항상 지울 것
+
+tel_key = '텔레그램 봇 키 입력'
+tel_id = '텔레그램 아이디 입력'
